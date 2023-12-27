0beba05ec17a69ac1314f46f9626393a6164f0ae
Ashiq <108174129+Ashii1@users.noreply.github.com>
2023-12-27 19:36:03 +0530

    DataPage.js

--- src/DataPage.js
+++ src/DataPage.js
@@ -24,7 +24,7 @@ const DataPage = ({ onBack }) => {
   return (
     <div>
       <button onClick={onBack}>Back</button>
-      <h2>Data </h2>
+      <h2>Information </h2>
       
       {loading ? (
         <p>Gathering...</p>
